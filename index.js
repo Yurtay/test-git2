3abdb667a907b64b978316541ce135c23eeefc7e
Igor Yurtaev Work <igoryurtay1989@gmail.com>
2022-08-24 17:29:05 +0500

    study GIT

---
+++ index.js
@@ -0,0 +1,17 @@
+function isPalindrome(str) {
+  for (let  i= 0, j = str.length - 1; i < j; i++, j--) {
+    if(str[i] != str[j])
+    return console.log(false)
+  }
+  return console.log(true)
+}
+
+isPalindrome("racecar")
+isPalindrome('programmer')
+
+function isPalindrome2(str) {
+  return console.log(str == str.split('').reverse().join('')) //разделили строку на массив, развернули её, собрали обратно в строку
+}
+
+isPalindrome2("racecar")
+isPalindrome2('programmer')
